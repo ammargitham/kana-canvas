634b9db81bcc249c35735684ebb74997dd2f8fca
Ammar Githam <ammar.githam@outlook.com>
2025-03-02 19:40:42 +0900

    Add NotFound component

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+
+export default function NotFound() {
+  return (
+    <div className='flex h-[calc(100vh_-_56px)] w-full items-center justify-center p-6 md:p-10'>
+      <div className='flex flex-col gap-4'>
+        <h2 className='text-3xl font-bold'>Page Not Found</h2>
+      </div>
+    </div>
+  )
+}
