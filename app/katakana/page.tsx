41fb16f361f60362e3d8b5daced5c4d9a658f28e
Ammar Githam <ammar.githam@outlook.com>
2025-03-02 18:33:14 +0900

    Update Katakana page header

--- app/katakana/page.tsx
+++ app/katakana/page.tsx
@@ -12,7 +12,7 @@ export default function KatakanaPage() {
         alt='Katakana'
       />
       <main className='container mx-auto p-4 mt-4'>
-        <h1 className="text-3xl font-bold mb-4">Katakana Practice</h1>
+        <h1 className="text-3xl font-bold mb-8">Katakana</h1>
         <LetterGrid letters={letters.katakana} type="katakana" />
       </main>
     </div>
