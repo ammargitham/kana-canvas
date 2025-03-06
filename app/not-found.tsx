c99ba69fb59f2e65720e6f6ba593c7b90998a894
Ammar Githam <ammar.githam@outlook.com>
2025-03-06 20:42:50 +0900

    Update NotFound component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,8 +2,9 @@
 export default function NotFound() {
   return (
     <div className='flex h-[calc(100vh_-_56px)] w-full items-center justify-center p-6 md:p-10'>
-      <div className='flex flex-col gap-4'>
-        <h2 className='text-3xl font-bold'>Page Not Found</h2>
+      <div className='flex flex-col gap-2 items-center'>
+        <h2 className='text-3xl font-bold'>404</h2>
+        <h2 className='text-xl font-light'>Page Not Found</h2>
       </div>
     </div>
   )
